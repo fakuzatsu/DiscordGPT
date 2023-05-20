44ae82e6dbe431266f615424ca04581d452c0596
fakuzatsu <abyerswilson@gmail.com>
2023-05-20 15:03:49 +0100

    refactored

--- main.js
+++ main.js
@@ -11,7 +11,6 @@ const client = new Client({
     IntentsBitField.Flags.MessageContent,
   ],
 });
-
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
@@ -24,71 +23,63 @@ const configuration = new Configuration({
 const openai = new OpenAIApi(configuration);
 
 
-// Preforms the GPT call and returns the result.
+// Fetches previous messages for context.
+async function fetchPreviousMessages(message, messageLimit) {
+    const prevMessages = await message.channel.messages.fetch({ limit: messageLimit });
+    const filteredMessages = prevMessages
+      .filter((msg) => !msg.content.startsWith('!') && !msg.author.bot)
+      .map((msg) => ({
+        role: 'user',
+        content: `${msg.author.username}: ${msg.content}`,
+      }));
+    return filteredMessages.reverse();
+  }
+
+
+  // Preforms the GPT call and returns the result.
 async function sendGPTRequest(message, client, conversationLog, messageLimit = 15) {
-  try {
-    await message.channel.sendTyping();
-
-    if (messageLimit > 0) {
-      // The bot reads the previous 15 messages in the channel for context.
-      let prevMessages = await message.channel.messages.fetch({ limit: messageLimit });
-      prevMessages.reverse();
-
-      // The bot ignores messages starting with ! and other bot or server messages.
-      prevMessages.forEach((msg) => {
-        if (message.content.startsWith('!')) return;
-        if (msg.author.id !== client.user.id && message.author.bot) return;
-
-        conversationLog.push({
-          role: 'user',
-          content: `${msg.author.username}: ${msg.content}`,
-        });
-      });
-    }
-
-    // The bot contacts openai to get its response.
-    const result = await openai
-      .createChatCompletion({
+    try {
+      await message.channel.sendTyping();
+  
+      if (messageLimit > 0) {
+        const prevMessages = await fetchPreviousMessages(message, messageLimit);
+        conversationLog.push(...prevMessages);
+      }
+  
+      const result = await openai.createChatCompletion({
         model: 'gpt-3.5-turbo',
         messages: conversationLog,
         max_tokens: 256, // limit token usage to around 100 words.
-      })
-      .catch((error) => {
-        console.log(`OPENAI ERR: ${error}`);
       });
-
-    // Turn the response into a string.
-    const response = result.data.choices[0].message;
-    const responseAsString = response['content'].toString();
-
-    // Check if the bot started it's sentence like "Bot:" and trims it off if so.
-    const name = client.user.username; 
-    const namePattern = new RegExp (`^${name}:`);
-    const finalResponse = responseAsString.replace(namePattern, '').trim();
-
-    // Returns the final response.
-    return finalResponse;
-  } catch (error) {
-    console.log(`ERR: ${error}`);
-    message.reply(`Sorry! I couldn't contact OpenAI. Please try again shortly.`)
+  
+      const response = result.data.choices[0].message;
+      let finalResponse = response.content.trim();
+  
+      // Remove the bot's name from the response if present.
+      const name = client.user.username.toLowerCase();
+      const botNamePattern = new RegExp(`^${name}:`, 'i');
+      if (finalResponse.toLowerCase().startsWith(name.toLowerCase() + ':')) {
+        finalResponse = finalResponse.replace(botNamePattern, '').trim();
+      }
+  
+      return finalResponse;
+    } catch (error) {
+      console.log(`ERR: ${error}`);
+      message.reply(`Sorry! I couldn't contact OpenAI. Please try again shortly.`);
+    }
   }
-}
 
 
-// Begins the process when a message is sent in the chat. Omits bot and server messages, as well as messages beginning with !.
+  // Begins the process when a message is sent in the chat. Omits bot and server messages, as well as messages beginning with !.
 client.on('messageCreate', async (message) => {
-  if (message.author.bot) return;
-  if (message.content.startsWith('!')) return;
+    if (message.author.bot || message.content.startsWith('!')) return;
+  
+    const name = client.user.username.toLowerCase();
+    if (message.content.toLowerCase().includes(name)) {
+      const conversationLog = [{ role: 'system', content: `You are an AI assistant called ${name}.` }];
+      const GPTResponse = await sendGPTRequest(message, client, conversationLog);
+      message.reply(GPTResponse);
+    }
+  });
 
-  // Only lets the bot speak when mentioned.
-  const name = client.user.username.toLowerCase();
-  if (message.content.toLowerCase().includes(name)) {
-
-    let conversationLog = [{ role: 'system', content: `You are an AI assistant called ${name}.`}];
-    const GPTResponse = await sendGPTRequest(message, client, conversationLog);
-    message.reply(GPTResponse);
-
-  }
-}); 
-
-client.login(process.env.DISC);
+  client.login(process.env.DISC);
